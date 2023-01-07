9d70b946170ba418fae8e6a9f951919f4227bfb4
rival14 <wijokumoroo@gmail.com>
2023-01-07 10:37:31 +0700

    stock on complete & on borrow

--- app/Http/Controllers/admin/OrdersController.php
+++ app/Http/Controllers/admin/OrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Buku;
 use App\Models\Order;
 use Illuminate\Http\Request;
 
@@ -22,6 +23,10 @@ class OrdersController extends Controller
         Order::where('id', $id)->update([
             'status' => 'Selesai'
         ]);
+
+        $order = Order::select('id', 'buku_id')->where('id', $id)->first();
+
+        Buku::where('id', $order->buku_id)->increment('stock');
 
         return redirect('/dashboard/order')->with("berhasil", "Sukses mengubah status peminjaman!!.");
     }
